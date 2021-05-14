1f2e28e8bd36c8c5dbbc9ff307565549732d3819
JustNunes <83143675+SdoakNunes@users.noreply.github.com>
2021-05-14 17:18:24 +0100

    Update ipsweep.sh

--- ipsweep.sh
+++ ipsweep.sh
@@ -1,3 +1,5 @@
+#This searches for the last three digits of nearby ip's from range 1 to 254.
+
 #!/bin/bash
 for ip on seq 1 254 ; do
 ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
